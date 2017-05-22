dcc6cba34bb72e470820e63e4d065dbed4695a33
Nick Freear <nfreear@yahoo.co.uk>
2017-05-22 16:58:46 +0100

    Bug #1, v2.x - initial commit new `sign-machine` JS, LESS [iet:9046906]
    * Plus `package` JSON, Travis-CI YAML

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 
+[![Build status — Travis-CI][travis-icon]][travis]
+[![Javascript semistandard style][semi-icon]][semi]
+
 # nfreear / sign-machine
 
 ## Version 2.x
 
 Javascript to finger-spell using sign language. Currently supports British Sign Language.
 
-* Javascript, LESS, CSS, HTML.
+* Javascript, LESS, CSS, HTML, GIF,
+* GIF images: each is animated, with 3 frames, 100 x 100 pixels.
 
 ```
 graphics/*.gif
@@ -17,26 +21,28 @@ index.html
 package.json
 ```
 
-### Usage:
+### Usage
+
+HTML web-page snippet:
 
 ```html
-<!doctype html>
-
 <div id="id-sign-machine"></div>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
 <script src="src/sign-machine.js"></script>
 ```
 
+[![British Sign Language][bsl-img]][bsl]
+
 ---
 
 ## Version 1.x
 
-### Original
+### Legacy
 
 British Sign Language finger-spelling machine - web service.
 
-* PHP, Javascript, CSS.
+* PHP, Javascript, CSS, GIF.
 
 ```
 bookmarklet/index.php
@@ -51,21 +57,38 @@ index.php
 * [Compatibility: PHP 4, IE 6, Firefox 3, Safari.]
 * @author N.D.Freear, 29 August-22 September.
 * @copyright Copyright 2008 N.D.Freear.
-- Each GIF, 3 frames, 100x100 pixels.
 
 > Initial commit of sign-machine aka. "BSM". Author/ Copyright 2008-08-30 Nicholas Freear (except graphics).
 
 
 * GitHub: [nfreear/sign-machine][gh]
 * Bitbucket: [nfreear/sign-machine][bit]
-* 1.x PHP demo: [freear.org.uk/sign?text=Hello][php]
+* v1.x PHP demo: [freear.org.uk/sign?text=Hello][php]
 
 ---
-© 2008-2017 Nick Freear.
+© 2008-2017 [Nick Freear][blog]. All rights reserved. (`UNLICENSED` _for now_.) [@nfreear][].
 
 
 [gh]: https://github.com/nfreear/sign-machine
 [bit]: https://bitbucket.org/nfreear/sign-machine
 [php]: http://freear.org.uk/sign/?text=Hello%21
+[@nfreear]: https://twitter.com/nfreear "Twitter: @nfreear"
+[blog]: http://nick.freear.org.uk "Nick Freear's blog"
+
+[RawGit]: https://rawgit.com/
+    "Serves Git files with the correct mime-type; content delivery network (CDN)"
+[travis]: https://travis-ci.org/nfreear/sign-machine "Build status – Travis-CI (NPM/eslint)"
+[travis-icon]: https://api.travis-ci.org/nfreear/sign-machine.svg
+[semi]: https://github.com/Flet/semistandard "Javascript coding style — 'semistandard'"
+[semi-icon]: https://img.shields.io/badge/code%20style-semistandard-brightgreen.svg?style=flat-square
+
+[bsl]: https://en.wikipedia.org/wiki/British_Sign_Language "British Sign Language"
+[bsl-img-0]: https://commons.wikimedia.org/wiki/File:BSL_Name.png
+[bsl-img]: https://upload.wikimedia.org/wikipedia/commons/d/d8/BSL_Name.png
+[sl-inter]: https://commons.wikimedia.org/wiki/File:Pictograms-nps-accessibility-sign_language_interpretation-2.svg
+[asl-img]: https://commons.wikimedia.org/wiki/File:Sign_language_A.svg
+[asl-2]: https://wpclipart.com/sign_language/American_ABCs/
+[signing]: https://pixabay.com/en/sign-language-deaf-gesture-signing-28716/
+[gov]: https://github.com/UKHomeOffice/posters/blob/master/accessibility/posters_en-UK/deaf.pdf
 
 [End]: //
---
+++ src/sign-machine.js
@@ -0,0 +1,120 @@
+/*!
+  sign-machine Version 2.
+
+  © Nick Freear, 21-May-2017.
+*/
+
+window.jQuery(function ($) {
+  'use strict';
+
+  var defaults = {
+    id: 'id-sign-machine',
+    timer: 1400, // Milli-seconds.
+    lang_sign: 'sgn-GB', // OR: 'bfi'
+    lang: 'en',
+    label: 'British Sign Language finger spelling.',
+    ignore_re: /[^a-zA-Z]/g,
+    value: 'Hello!',
+    form: '<form><label>Finger spell this <input name="sign" value="%s" required ></label><input type="submit" value="Sign"></form><p></p>',
+    graphic: '<img src="%u/../../graphics/%s.gif" alt="Letter: %s" title="Letter: %s" class="letter">',
+    space: '<i class="space" aria-label="space"></i>',
+    script: 'sign-machine.js',
+    dev: false,
+    css_url: '/../../style/sign-machine.css',
+    css_link: '<link rel="stylesheet" href="%s" />',
+    less_url: '/../../style/sign-machine.less',
+    less_link: '<link rel="stylesheet/less" type="text/css" href="%s" />',
+    less_js: '<script src="https://cdnjs.cloudflare.com/ajax/libs/less.js/2.7.2/less.min.js"></script>'
+  };
+
+  var SM = defaults;
+
+  var W = window;
+  var C = W.console;
+
+  var $script = $('script[ src *= "' + SM.script + '"]').first();
+
+  SM.script_url = $script.attr('src');
+
+  addStylesheet();
+
+  var $CTR = $('#' + SM.id);
+
+  $CTR
+    .html(SM.form.replace(/%s/, SM.value))
+    .addClass('sign-machine-v2-js')
+    .attr({ role: 'alert', 'data-sl': SM.lang_sign, 'aria-label': SM.label })
+    // .append('<a href="' + SM.script_url + '" class="abs-url">xx</a>')
+    ;
+
+  var $form = $CTR.find('form');
+  var $input = $form.find('input[ name = sign ]');
+  var $inner = $CTR.find('p');
+
+  $form.on('submit', function (ev) {
+    ev.preventDefault();
+
+    var phrase = $input.val();
+
+    signThis(phrase);
+  });
+
+  parseQuery(W.location.search);
+
+  C.warn('SM: sign-machine-v2: ', SM);
+
+  // ----------------------------------------
+
+  var interval;
+
+  function signThis (phrase) {
+    var ignorecount = (phrase.match(SM.ignore_re) || [ ]).length;
+    var letters = SM.letters = phrase.replace(SM.ignore_re, ' ').split('');
+    var idx = 0;
+
+    W.clearInterval(interval);
+
+    interval = W.setInterval(function () {
+      var letter = letters[ idx ];
+      var graphic;
+
+      if (idx > letters.length || !letter) {
+        return W.clearInterval(interval);
+      }
+
+      if (letter === ' ') {
+        graphic = SM.space;
+      } else {
+        graphic = SM.graphic.replace(/%u/, SM.script_url).replace(/%s/g, letter.toLowerCase());
+      }
+
+      $inner.html(graphic);
+
+      C.warn('SM letter: "%s", %s', letter, graphic);
+
+      idx++;
+    },
+    SM.timer);
+
+    C.warn('SM: request: ', letters, letters.length, ignorecount);
+  }
+
+  function parseQuery (params) {
+    var mparam = params.match(/[&?]sign=([^&]+)/);
+    if (mparam) {
+      $input.val(W.decodeURIComponent(mparam[ 1 ]));
+      $form.trigger('submit');
+    }
+  }
+
+  function addStylesheet () {
+    var $head = $('head');
+    if (SM.dev) {
+      $head
+        .append(SM.less_js)
+        .append(SM.less_link.replace(/%s/, SM.script_url + SM.less_url));
+    } else {
+      $head.append(SM.css_link.replace(/%s/, SM.script_url + SM.css_url));
+    }
+  }
+});
